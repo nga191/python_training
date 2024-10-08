8921341df8456b87227c45e2dbfcabbe08afebbf
nga191 <ttnga191@gmail.com>
2024-10-08 17:51:43 +0700

    init commit

---
+++ day1/main.py
@@ -0,0 +1,14 @@
+# Ctrl + ,
+print("Hello World")
+print("My name is Nga")
+# Vào Setting/Extension/ Run code configuration tích chọn Clear Previous Code 
+# -> Để lxóa sạch những kết quả đã chạy trước đó
+# Win + .: Để hiển thị bảng biểu tượng cảm xúc
+# Để chạy ra biểu tượng cảm xúc cần vào Setting/Extension/ Run code configuration
+# -> Excuter Map: Ở dòng Python: Sửa lại thành: set PYTHONIOENCODING=utf8 && python -u
+print("❤️🤣")
+    
+ 
+
+
+
